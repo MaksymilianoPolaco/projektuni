42d43033a1811e841e93b0137b990e7a4f63d1db
mac <mac@Maria-Jesus.local>
2025-07-14 20:29:27 -0400

    Kolejnie zmiany polecenie 4

--- projekt1.cpp
+++ projekt1.cpp
@@ -1,3 +1,18 @@
+int main() {
+    int choice;
+    std::cout << "Menu:\n";
+    std::cout << "1) Oblicz silnię\n";
+    std::cout << "2) Wyjście\n";
+    std::cout << "Wybierz opcję: ";
+    std::cin >> choice;
+
+    switch (choice) {
+        case 1: {
+            int n;
+            std::cout << "Podaj n: ";
+            std::cin >> n;
+
+
 unsigned long long factorial(int n) {
 
     if (n < 0) return 0; 
@@ -15,3 +30,16 @@ unsigned long long factorial(int n) {
     return result;
 
 }
+
+         unsigned long long result = factorial(n);
+
+         std::cout << "Silnia(" << n << ") = " << result << std::endl;
+         break;
+     }
+     case 2:
+         return 0;
+     default:
+         std::cout << "Nieprawidłowa opcja\n";
+ }
+ return 0;
+
